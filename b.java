496b3096cbb3e69235291f890a4f7077b6db1c7d
Reshmamali <reshmamali720@gmail.com>
2024-12-22 21:15:42 +0530

    New changes in b.java

--- b.java
+++ b.java
@@ -1,1 +1,6 @@
-hgcjsabclksacs svsvds
+public class HelloWorld{
+    public static void main(String []args){
+        System.out.println("I am writting this code");
+        System.out.println("Hello world")
+    }
+}
