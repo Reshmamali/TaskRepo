77bb03ef975cda412346864a0570521d8fe7d53a
Reshmamali <reshmamali720@gmail.com>
2024-12-23 14:59:08 +0530

    Commiting another chages in 2.java file

--- 2.java
+++ 2.java
@@ -1,19 +1,3 @@
-public class palimdrome{
-    public static void main(String []args){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a string -> ");
-        String str = sc.nextInt();
-        String reverse = "";
+<<<<<<<<<<<<<<<<<<HEAD
+First you add a file, but creamvoiidsnvoid
 
-        for(int i = 1; i<=a.length()-1; i++){
-            reverse = reverse + a.charAt(i);
-        }
-        System.out.print("Reverse string is" + reverse);
-
-        if(reverse.equals(a)){
-            System.out.println("String is palindrome");
-        }else{
-            System.out.println("String is not palindrome");
-        }
-    }
-}
